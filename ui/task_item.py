d185929ae3dbf7f461e76499d52dcd952f958da9
Angelo <sanchez.angel.informatique@gmail.com>
2025-11-21 20:11:14 +0100

    Patch1.2
    Co-authored-by: BenMichalak21 <ben.michalak2008@gmail.com>

--- ui/task_item.py
+++ ui/task_item.py
@@ -9,19 +9,19 @@ class TaskItem:
     def __init__(self, surface):
         self.surface = surface
         self.liste_tasks = Fonctionnalite()
-        self.width = 100
-        self.height = 50
-        self.x = self.surface.get_width() // 2
+        self.width = self.surface.get_width() // 3
+        self.height = 75
+        self.x = self.surface.get_width() // 3
         self.y = 0
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def afficher_task(self):
         self.y = 0
         self.font_task = pygame.font.SysFont("Calibri", 20)
         for cle in self.liste_tasks.liste.keys():
-            self.y += 20
-            pygame.draw.rect(self.surface, (0, 0, 0), self.rect)
-            self.txt_task = self.font_task.render(cle, True, (0, 0, 0))
-            self.pos_task = self.txt_task.get_rect()
-            self.pos_task.centerx = self.surface.get_rect().centerx
-            self.pos_task.centery = 20
+            self.y += 75
+            self.rect_task = pygame.draw.rect(
+                self.surface, (66, 0, 0), [self.x, self.y, self.width, self.height], 2
+            )
+            self.txt_task = self.font_task.render(cle, True, (66, 0, 0))
+            self.pos_task = self.txt_task.get_rect(center=self.rect_task.center)
+            self.surface.blit(self.txt_task, self.pos_task)
